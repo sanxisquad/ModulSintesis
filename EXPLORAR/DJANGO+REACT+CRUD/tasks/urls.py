9807c0fff58fa0b390cf83b9ead8fedad3686ce0
sanxisquad <msancheztasies@gmail.com>
2025-03-04 08:48:54 +0100

    CRUD backend

---
+++ EXPLORAR/DJANGO+REACT+CRUD/tasks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
+from tasks import views
+
+router=routers.DefaultRouter()
+router.register(r'tasks',views.TaskViewSet, 'tasks')
+
+urlpatterns = [
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title='Tasks API'))
+
+]
